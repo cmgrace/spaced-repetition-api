b2a18f4e39a5630925ab1ecf4003a1927116c438
Ming Cheng <cmgrace@bu.edu>
2021-03-08 14:55:27 -0500

    first commit

---
+++ src/middleware/error-handler.js
@@ -0,0 +1,10 @@
+const { NODE_ENV } = require('../config');
+
+module.exports = function errorHandler(error, req, res, next) {
+  const response =
+    NODE_ENV === 'production'
+      ? { error: 'Server error' }
+      : { error: error.message, details: error };
+  console.error(error);
+  res.status(500).json(response);
+};
